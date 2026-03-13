ea0b485501444438f5c1e6d510eea56adf7e9c7a
Freud <freud@wrmsoftware.com>
2026-03-14 00:13:55 +0700

    fix: delete js file build artifacts

--- packages/shared-schema/src/dto/login-user.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginUserSchema = void 0;
-const zod_1 = require("zod");
-const validators_1 = require("../validators");
-exports.loginUserSchema = zod_1.z.object({
-    username: validators_1.usernameSchema,
-    password: validators_1.passwordSchema,
-});
