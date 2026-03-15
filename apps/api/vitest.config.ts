22823b55eb00b3d06e7caee55cd920978ee33d6e
Freud <freud@wrmsoftware.com>
2026-03-15 15:30:32 +0700

    fix

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,15 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    passWithNoTests: true,
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'commonjs' },
+    }),
+  ],
+});
